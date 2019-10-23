f0c178dac188e9ee3b8ade5d863300f4bb9c2163
kee-mun <jeongyun913@gmail.com>
2019-10-23 17:51:31 +0900

    add comboBox for Select Year or Month

--- GUIProgram/InterestCalc/InterestCalc.py
+++ GUIProgram/InterestCalc/InterestCalc.py
@@ -12,11 +12,35 @@ class myWin(QWidget):
         self.move(300,300)
         self.resize(400,200)
 
-        exbtn = QPushButton("닫기", self)
-        exbtn.move(150, 150)
-        exbtn.resize(exbtn.sizeHint())
-        exbtn.clicked.connect(QCoreApplication.instance().quit)
+        #####기간 선택하기 (월/년)
+        self.lbl = QLabel(self)
+        self.lbl.setText("1. 기간 :")
+        self.lbl.move(0,0)
+        self.lbl.resize(45,20)
 
+        self.tiEdit = QLineEdit("",self)
+        self.tiEdit.move(50,0)
+        self.tiEdit.resize(45,20)
+
+        self.opcb = QComboBox(self)
+        self.opcb.move(100,0)
+        self.opcb.addItem("Month")
+        self.opcb.addItem("Year")
+        self.opcb.currentIndexChanged.connect(self.setTime)
+
+        #####이자율 입력
+        exbtn1 = QPushButton("닫기", self)
+        exbtn1.move(190, 150)
+        exbtn1.resize(exbtn1.sizeHint())
+        exbtn1.clicked.connect(QCoreApplication.instance().quit)
+
+        exbtn2 = QPushButton("계산", self)
+        exbtn2.move(110, 150)
+        exbtn2.resize(exbtn2.sizeHint())
+        #exbtn2.clicked.connect(QCoreApplication.instance().quit)
+
+    def setTime(self):
+        self.time = self.opcb.currentText()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
