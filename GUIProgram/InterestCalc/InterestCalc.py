26e27db0cc744fc88078c4117754644de8986ea3
kee-mun <jeongyun913@gmail.com>
2019-10-23 16:13:40 +0900

    Add Close Button

--- GUIProgram/InterestCalc/InterestCalc.py
+++ GUIProgram/InterestCalc/InterestCalc.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QCoreApplication
 
 class myWin(QWidget):
     def __init__(self):
@@ -10,6 +11,12 @@ class myWin(QWidget):
         self.setWindowTitle('이자율 계산기')
         self.move(300,300)
         self.resize(400,200)
+
+        exbtn = QPushButton("닫기", self)
+        exbtn.move(150, 150)
+        exbtn.resize(exbtn.sizeHint())
+        exbtn.clicked.connect(QCoreApplication.instance().quit)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
