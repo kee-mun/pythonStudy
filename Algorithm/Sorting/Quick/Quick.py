7472f400a71c2482e4757aa151b04cc0c1eff4d2
kee-mun <jeongyun913@gmail.com>
2019-11-28 17:57:08 +0900

    Quick Sorting Algorithm

---
+++ Algorithm/Sorting/Quick/Quick.py
@@ -0,0 +1,14 @@
+#Quick Sorting Algorithm에 대해서
+#퀵소트알고리즘은 Unsorted List에서 임의의 Pivot값을 정해
+#Pivot 값보다 작은 값은 PIvot의 왼쪽으로
+#Pivot 값보다 큰 값은 Pivot의 오른쪽으로 보내서 데이터 정렬하는 방식
+
+# Quick Sorting Algorithm의 O()는
+# 최선 O(n Log(n)) 최악 O(n^2) 평균 O(n log(n))의 복잡도를 가진다.
+
+def quicksorted(arr):
+    pass
+
+
+arr = [1,2,5,4,3,8,7,5]
+print(quicksorted(arr))
